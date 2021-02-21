489dfc74addce0279fa530970d7123b97f8f6f0c
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2021-02-21 19:52:06 +0200

    updating VoiceClient.py

--- Voice/VoiceClient.py
+++ Voice/VoiceClient.py
@@ -1,16 +1,24 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 import pyaudio
+import queue
+import threading
 
 
 class Client(DatagramProtocol):
-    def __init__(self):
+    def __init__(self, ip, port, command_queue):
+        self.another_client = ip, port
         self.buffer = 1024  # 127.0.0.1
         self.mute = False
+        self.output_stream = None
+        self.input_stream = None
+        self.message_to_mute = "mute please"
+        self.message_queue = queue.SimpleQueue()
+        for element in command_queue:
+            self.message_queue.put(element)
 
     def startProtocol(self):
         py_audio = pyaudio.PyAudio()
-        self.another_client = input("Write address: "), int(input("Write port: "))
 
         self.output_stream = py_audio.open(format=pyaudio.paInt16,
                                            output=True, rate=44100, channels=2,
@@ -18,12 +26,17 @@ class Client(DatagramProtocol):
         self.input_stream = py_audio.open(format=pyaudio.paInt16,
                                           input=True, rate=44100, channels=2,
                                           frames_per_buffer=self.buffer)
-        reactor.callInThread(self.record)
+        # reactor.callInThread(self.record)
+        # not sure if needed
 
     def record(self):
         while not self.mute:
             data = self.input_stream.read(self.buffer)
             self.transport.write(data, self.another_client)
+            if not self.message_queue.empty():
+                message = self.message_queue.get()
+                if message == self.message_to_mute:
+                    self.Mute()
 
     def datagramReceived(self, datagram, address):
         self.output_stream.write(datagram)
