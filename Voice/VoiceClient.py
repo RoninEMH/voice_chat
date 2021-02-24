b0a3b6a0bd23c479af589943d3f63c4d1fb189c1
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2021-02-24 19:17:32 +0200

    updating VoiceClient.py messages and working threads

--- Voice/VoiceClient.py
+++ Voice/VoiceClient.py
@@ -5,17 +5,19 @@ import queue
 import threading
 
 
-class Client(DatagramProtocol):
+class VoiceClient(DatagramProtocol):
     def __init__(self, ip, port, command_queue):
         self.another_client = ip, port
         self.buffer = 1024  # 127.0.0.1
         self.mute = False
         self.output_stream = None
         self.input_stream = None
+        self.message_queue = command_queue
+        self.initialize_messages()
+
+    def initialize_messages(self):
         self.message_to_mute = "mute please"
-        self.message_queue = queue.SimpleQueue()
-        for element in command_queue:
-            self.message_queue.put(element)
+        self.message_to_abort = "abort please"
 
     def startProtocol(self):
         py_audio = pyaudio.PyAudio()
@@ -34,20 +36,34 @@ class Client(DatagramProtocol):
             channels=2,
             frames_per_buffer=self.buffer,
         )
-        # reactor.callInThread(self.record)
-        # not sure if needed
+        reactor.callInThread(self.record)
+        self.pull_from_queue()
 
     def record(self):
         while not self.mute:
             data = self.input_stream.read(self.buffer)
             self.transport.write(data, self.another_client)
-            if not self.message_queue.empty():
-                message = self.message_queue.get()
-                if message == self.message_to_mute:
-                    self.Mute()
 
     def datagramReceived(self, datagram, address):
         self.output_stream.write(datagram)
 
     def Mute(self):
         self.mute = True
+
+    def check_queue(self):
+        while True:
+            if not self.message_queue.empty():
+                message = self.message_queue.get()
+                if message == self.message_to_mute:
+                    self.Mute()
+                if message == self.message_to_abort:
+                    self.Mute()  # just for safety
+                    break
+
+    def pull_from_queue(self):
+        threading.Thread(target=self.check_queue(), args=(), daemon=True)
+
+
+def run(ip, port, messages_queue):
+    reactor.listenUDP(port, VoiceClient(ip, port, messages_queue))
+    reactor.run()
