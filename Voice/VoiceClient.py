df0b905d9fddbce8de569226ad519d87ed9b76a9
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2021-02-25 22:19:28 +0200

    finishing VoiceClient.py yeah

--- Voice/VoiceClient.py
+++ Voice/VoiceClient.py
@@ -3,7 +3,6 @@ from twisted.internet import reactor
 import pyaudio
 import queue
 import threading
-import time
 
 
 class VoiceClient(DatagramProtocol):
@@ -73,42 +72,6 @@ class VoiceClient(DatagramProtocol):
                     reactor.stop()  # not in try because we want to stop anyway
                     break
 
-
-def run(ip, port, messages_queue):
-    reactor.listenUDP(port, VoiceClient(ip, port, messages_queue))
-    reactor.run()
-
-
-def example1():
-    print("beginning example1")
-    answer = "mute"
-    q.put(answer)
-    time.sleep(2)
-    answer = "unmute"
-    q.put(answer)
-    time.sleep(2)
-    answer = "abort"
-    q.put(answer)
-    print("example1 ended")
-
-
-def example2():
-    print("beginning example2")
-    answer = input("command: ")
-
-    while True:
-        q.put(str(answer))
-        if str(answer) == "abort":
-            break
-        answer = input("command: ")
-
-    print("example2 ended")
-
-
-q = queue.SimpleQueue()
-
-# examples to check the voice client
-# threading.Thread(target=example1, daemon=True).start()
-# threading.Thread(target=example2, daemon=False).start()
-
-run("127.0.0.1", 2565, q)
+    def run(self):
+        reactor.listenUDP(self.another_client[1], self)
+        reactor.run()
