d42fc543575fd841c4da4343237e5ac5ab223f8b
EyalEliav <eyaleliav15@gmail.com>
2021-03-07 17:36:26 +0200

    initialize main CLI

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from random import randint
 
 
 @click.command("Create")
-@click.option('--name', '-n', required=True)
-@click.option('--voice', '-v', default=randint(1000, 1500), type=int)
-@click.option('--chat', '-c', default=randint(1500, 2000), type=int)
+@click.option("--name", "-n", required=True)
+@click.option("--voice", "-v", default=randint(1000, 1500), type=int)
+@click.option("--chat", "-c", default=randint(1500, 2000), type=int)
 def create(name, voice, chat):
     print(f"Name : {name} Voice : {voice} Chat : {chat}")
 
 
 @click.command("Join")
-@click.option('--name', '-n', required=True)
-@click.option('--host', '-h', required=True)
-@click.option('--voice', '-v', default=randint(1000, 1500), type=int)
-@click.option('--chat', '-c', default=randint(1500, 2000), type=int)
+@click.option("--name", "-n", required=True)
+@click.option("--host", "-h", required=True)
+@click.option("--voice", "-v", default=randint(1000, 1500), type=int)
+@click.option("--chat", "-c", default=randint(1500, 2000), type=int)
 def join(name, host, voice, chat):
     print(f"Name : {name} Host : {host} Voice : {voice} Chat : {chat}")
 
